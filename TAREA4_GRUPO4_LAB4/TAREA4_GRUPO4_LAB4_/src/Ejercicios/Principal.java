91481eb62881cd8ae5c02b5cf3bc63ce2ddae124
davidchap <116680803+dadevsoft@users.noreply.github.com>
2025-11-03 17:54:45 -0300

    Update Principal.java

--- TAREA4_GRUPO4_LAB4/TAREA4_GRUPO4_LAB4_/src/Ejercicios/Principal.java
+++ TAREA4_GRUPO4_LAB4/TAREA4_GRUPO4_LAB4_/src/Ejercicios/Principal.java
@@ -1,12 +1,2 @@
-package Ejercicios;
 
-public class Principal {
-
-	public static void main(String[] args) {
-		
-		 VentanaPrincipal v = new VentanaPrincipal();
-	     v.CambiarVisibilidad(true);
-	}
-
-}
  
